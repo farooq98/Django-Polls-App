9ae0f3853c8427e5a2e524916671faae783e9fa1
farooq98 <farooqhd98@gmail.com>
2021-01-16 18:48:30 +0500

    BootStrap Added

--- webapp/djangolab/views.py
+++ webapp/djangolab/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
+
+data = [
+    {
+        "id": 1,
+        "date" : "11/12/2020",
+        "question": "What is your name?",
+    },
+    {
+        "id": 2,
+        "date" : "01/01/2021",
+        "question": "What is your firend name?",
+    },
+    {
+        "id": 3,
+        "date" : "28/08/2020",
+        "question": "What is your favourite programming Language?",
+    },
+]
+
 def show(request):
-    return render(request, 'djangolab/index.html')
+    return render(request, 'djangolab/index.html', {"data":data})
 
 def about(request):
     return render(request, 'djangolab/about.html')
